6323cb6b47c9bb12db2fdb95708e22479963351e
Prashant Thakur <prashantthakur2001@gmail.com>
2023-11-09 23:49:38 +0530

    landing page

---
+++ src/component/Header.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import { Button, HStack, Image} from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+import img from "../assets/Add a heading (1).png"
+const bcss={
+  color: "black",
+  variant: "unstyled",
+  padding: "1rem",
+}
+const styling={
+    display: "flex",
+    justifyContent: "space-evenly",
+    width: "100%",
+}
+const book={
+    width:"auto",
+    color: "#fff",
+    borderRadius: "6px!important",
+    textTransform: "uppercase",
+    fontSize: "100%",
+    fontWeight: "400",
+    background: "#fc2361",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    verticalAlign: "middle",
+    cursor: "pointer",
+    userSelect: "none",
+    border: "1px solid transparent",
+    lineHeight: "1.5",
+}
+const custom={
+    border: "1px solid #fc2361",
+    background: "white",
+    color: "black",
+    fontWeight: "500",
+}
+const imgstyler={
+    objectFit: "contain",
+    maxWidth: "121px",
+    userSelect: "none",
+    draggable: "false",
+}
+const Header = () => {
+  return (
+    <HStack {...styling}>
+    <HStack>
+        <Image {...imgstyler} src={img}/>
+    </HStack>
+   <HStack  p={4}  >
+   
+    <Button _hover={{transform:"scale(1.2)"}} {...bcss}>
+      <Link  to="/">Home</Link>
+    </Button>
+    <Button _hover={{transform:"scale(1.2)"}} {...bcss}>
+      <Link  to="/services">Services</Link>
+    </Button>
+    <Button _hover={{transform:"scale(1.2)"}} {...bcss}>
+      <Link  to='/about'>Contact Us</Link>
+    </Button>
+    <Button _hover={{transform:"scale(1.2)"}} {...bcss}>
+      <Link  to='/join'>Join Us</Link>
+    </Button>
+    <Button _hover={{transform:"scale(1.2)"}} {...bcss}>
+      <Link  to='/ambulance'>Ambulances</Link>
+    </Button>
+    <Button _hover={{transform:"scale(1.2)"}} {...bcss}>
+      <Link  to='/hospital'>Hospitals</Link>
+    </Button>
+   </HStack>
+   <HStack>
+   <Button _hover={{
+        background: "rgb(221, 42, 87)" 
+      }}  {...book}>
+      <Link to='/book'>Book Now</Link>
+    </Button>
+    <Button _hover={{
+        borderColor: "#31c5fe",
+        textColor: "#31c5fe"
+      }} {...book} {...custom}>
+      <Link to='/login'>Login</Link>
+    </Button>
+   </HStack>
+   </HStack>
+  )
+}
+
+export default Header
